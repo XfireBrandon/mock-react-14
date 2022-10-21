e3f3d5ba310452787a83861aa90106460b372967
Brandon Martin <brandonmartin@Brandons-MacBook-Pro.local>
2022-10-21 15:36:23 -0600

    not finished

--- src/components/PostItem.js
+++ src/components/PostItem.js
@@ -1,7 +1,18 @@
+import axios from 'axios'
+
 const PostItem = ({post}) => {
+
+        
+    const handleClick = async (e) => {
+        const data = await axios.get(`https://jsonplaceholder.typicode.com/posts/${e.target.id}`)
+        
+        console.log(data.data.title)
+    }
+
+
     return(
-        <div className="listItem">
-            <h1>{post.title}</h1>
+        <div onClick={handleClick} className="listItem" id={post.id} >
+            <h1 id={post.id}>{post.title}</h1>
             <p>{post.id}</p>
         </div>
     )
--- src/components/Posts.js
+++ src/components/Posts.js
@@ -2,7 +2,7 @@ import PostItem from './PostItem'
 
 const Posts = ({posts}) => {
     return posts.map((post) => (
-        <PostItem post={post} />
+        <PostItem post={post} key={post.id} />
     ))
 }
 
